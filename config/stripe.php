d4fcc9c6622c9ef340a36e13b58e8c73ee960560
College Project <student@example.com>
2026-05-17 11:20:20 +0545

    Untrack stripe.php to protect secret keys

--- config/stripe.php
+++
@@ -1,8 +0,0 @@
-<?php
-// config/stripe.php
-
-// Define Stripe API Keys
-define('STRIPE_PUBLISHABLE_KEY', 'pk_test_51TXwHaI9RsVtrRU9AN4WsyteVOwgDCVr3Ux4Aa08dc7gy5bi7jpRK8Uj8tS5xr2KZF52TUk6smH3rSZGjp9Nj2ra00hZG5Tchz');
-define('STRIPE_SECRET_KEY', 'sk_test_..._HIDDEN_DUE_TO_GITHUB_SECURITY_POLICY'); // Please replace locally!
-
-?>
